658e8187f0589a32e2dc1c4a4b6d281042265876
Jclark083 <jaime.clark@utp.ac.pa>
2022-07-18 11:05:38 -0500

    Dpto audio ortoo
    Ya todo

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float damage;
+    public AudioSource daño;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             collision.GetComponent<Health>().TakeDamage(damage);
+            daño.Play();
         }
     }
 
--- Assets/Scripts/Health/HealthFood.cs
+++ Assets/Scripts/Health/HealthFood.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class HealthFood : MonoBehaviour
 {
     [SerializeField] private float healthValue;
+    public AudioSource sounded;
+
+    private void start()
+    {
+
+       sounded = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             collision.GetComponent<Health>().AddHealth(healthValue);
-            gameObject.SetActive(false);
+            Destroy(gameObject, 0.5f);
+            sounded.Play();
+            
+            
         }
     }
 }
